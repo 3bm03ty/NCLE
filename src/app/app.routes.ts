571fb312b3880fc829c9d8d9b008c94b477cb8b1
3bm03ty <mohamedabdalmoty3@gmail.com>
2025-04-10 13:21:06 +0200

    feat: setup routing for app navigation
    - Configured routing in the app to navigate between pages
    - Added routes for Home and Subscription pages
    - Ensured smooth navigation between pages with proper routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'subscription',
+    loadComponent: () => import('./pages/subscription/subscription.page').then(m => m.SubscriptionPage)
   },
 ];
